f5140661a6de9d580ab9a72b33c9165ffe9b97c2
liyanchao2tytp <57429651+liyanchao2tytp@users.noreply.github.com>
2020-10-15 21:00:45 +0800

    注释掉了中间件的配置，改为实例型对单个路由拦截，新建了添加文章、更新文章、获取文章列表、删除文章、根据id获取文章的接口

--- app/router/admin.js
+++ app/router/admin.js
@@ -5,6 +5,11 @@ const adminauth = require("../middleware/adminauth");
 module.exports = app => {
   const { router, controller } = app
   const adminauth = app.middleware.adminauth()
-  router.post('/admin/login',controller.admin.main.login)
-  router.get('/admin/getTypeInfo',adminauth,controller.admin.main.getTypeInfo)
+  router.post('/admin/login', controller.admin.main.login)
+  router.get('/admin/getTypeInfo', adminauth, controller.admin.main.getTypeInfo)
+  router.post('/admin/addArticle', adminauth, controller.admin.main.addArticle)
+  router.post('/admin/updateArticle',adminauth,controller.admin.main.updateArticle)
+  router.get('/admin/getArticleList',adminauth,controller.admin.main.getArticleList)
+  router.get('/admin/delArticle/:id',adminauth,controller.admin.main.delArticle)
+  router.get('/admin/getArticleById/:id',adminauth,controller.admin.main.getArticleById)
 }
